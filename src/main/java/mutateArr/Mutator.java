f9818650113322f502058c3a262281ca32d24e48
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-09-12 23:11:31 +0300

    .

---
+++ src/main/java/mutateArr/Mutator.java
@@ -0,0 +1,18 @@
+package mutateArr;
+
+import java.util.stream.IntStream;
+
+public class Mutator {
+    public int[] mutate(int n, int[] a) {
+        if (0 == n || n > a.length || null == a || 0 == a.length) {
+            return new int[]{};
+        }
+
+        return IntStream.range(0, n)
+                .map(index -> 0 == index && n - 1 == index ? a[index]
+                        : 0 == index ? a[index] + a[index + 1] : n - 1 == index
+                        ? a[index - 1] + a[index]
+                        : a[index - 1] + a[index] + a[index + 1])
+                .toArray();
+    }
+}
---
+++ src/test/java/mutateArr/TestMutator.java
@@ -0,0 +1,36 @@
+package mutateArr;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMutator {
+    static Stream getInputData() {
+        return Stream.of(
+            Arguments.of(5, new int[]{4, 0, 1, -2, 3}, new int[]{4, 5, -1, 2, 1}),
+            Arguments.of(4, new int[]{4, 0, 1, -2}, new int[]{4, 5, -1, -1}),
+            Arguments.of(3, new int[]{4, 0, 1}, new int[]{4, 5, 1}),
+            Arguments.of(2, new int[]{4, 0}, new int[]{4, 4}),
+            Arguments.of(1, new int[]{4}, new int[]{4}),
+            Arguments.of(0, new int[]{}, new int[]{})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getInputData")
+    void shouldMutateArray(int length, int[] inputArray, int[] expectedArray) {
+        final var resultingArray = new Mutator().mutate(length, inputArray);
+
+        assertAll(
+            () -> assertEquals(length, resultingArray.length),
+            () -> assertArrayEquals(expectedArray, resultingArray)
+        );
+    }
+}
