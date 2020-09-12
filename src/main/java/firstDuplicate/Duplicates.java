e716f5ea5a9846dee65e59674ba7b43e39e1dfb4
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-09-12 22:29:55 +0300

    .

--- src/main/java/firstDuplicate/Duplicates.java
+++ src/main/java/firstDuplicate/Duplicates.java
@@ -6,19 +6,34 @@ import java.util.Comparator;
 public class Duplicates {
     public int findFirstValidDuplicate(int[] a) {
         if (null == a || 0 == a.length) {
-            return 0;
+            return -1;
         }
         final Object[] uniqueElements = Arrays.stream(a).parallel()
-                .distinct()
-                .boxed()
-                .toArray();
+            .distinct()
+            .boxed()
+            .toArray();
 
-        final Object[] validInput = Arrays.stream(a).parallel().boxed().toArray();
+//        final Object[] validInput = Arrays.stream(a).parallel()
+//            .boxed()
+//            .toArray();
+//        final Integer positionInArray = Arrays.stream(uniqueElements).parallel()
+//                .filter(item -> Arrays.asList(validInput).indexOf(item) != Arrays.asList(validInput).lastIndexOf(item))
+//                .map(item -> Arrays.asList(Arrays.stream(validInput).parallel()
+//                        .skip(Arrays.asList(validInput).indexOf(item) + 1)
+//                        .toArray()).indexOf(item) +
+//                        Arrays.asList(validInput).indexOf(item) +
+//                        1)
+//                .min(Comparator.comparingInt(item -> item))
+//                .orElse(-1);
         final Integer positionInArray = Arrays.stream(uniqueElements).parallel()
-                .filter(item -> Arrays.asList(validInput).indexOf(item) != Arrays.asList(validInput).lastIndexOf(item))
-                .map(item -> Arrays.asList(Arrays.stream(validInput).skip(Arrays.asList(validInput).indexOf(item) + 1).toArray()).indexOf(item) + Arrays.asList(validInput).indexOf(item) + 1)
-                .min(Comparator.comparingInt(item -> item))
-                .orElse(-1);
+            .filter(item -> Arrays.asList(a).indexOf(item) != Arrays.asList(a).lastIndexOf(item))
+            .map(item -> Arrays.asList(Arrays.stream(a).parallel()
+                .skip(Arrays.asList(a).indexOf(item) + 1)
+                .toArray()).indexOf(item) +
+                Arrays.asList(a).indexOf(item) +
+                1)
+            .min(Comparator.comparingInt(item -> item))
+            .orElse(-1);
         return positionInArray > 0 ? a[positionInArray] : -1;
     }
 }
--- src/test/java/firstDuplicate/TestDuplicates.java
+++ src/test/java/firstDuplicate/TestDuplicates.java
@@ -17,20 +17,23 @@ public class TestDuplicates {
             Arguments.of(new int[]{2, 4, 3, 5, 1}, -1),
             Arguments.of(new int[]{1, 1, 2, 2, 1}, 1),
             Arguments.of(new int[]{1, 1, 2, 2, 1,1, 1, 2, 2, 1,1, 1, 2, 2, 1,1, 1, 2, 2, 11, 1, 2, 2, 11, 1, 2, 2, 11, 1, 2, 2, 1}, 1),
-            Arguments.of(getOneMillionOfIntegers(), 3)
+            Arguments.of(getOneMillionOfIntegers(), 3),
+            Arguments.of(generateArrayOfSame(10000000), 1)
         );
     }
 
     private static int[] getOneMillionOfIntegers() {
-        return IntStream.range(0, 100)//0000)
+        return IntStream.range(0, 100)
                 .toArray();
+    }
+
+    private static int[] generateArrayOfSame(int length) {
+        return IntStream.generate(() -> 1).limit(length).toArray();
     }
 
     @ParameterizedTest
     @MethodSource("getInputData")
     void shouldReturnDuplicate(int[] input, int expectedResult) {
-//        final var input = new int[]{2, 1, 3, 5, 3, 2};
-//        final var expectedResult = 3;
         final var cut = new Duplicates();
 
         Assertions.assertEquals(expectedResult, cut.findFirstValidDuplicate(input));
