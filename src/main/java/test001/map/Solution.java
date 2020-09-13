b21069e3b1c5851f3a6ddb7fbe96ec3cba8ce0b3
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-09-13 22:40:32 +0300

    .

---
+++ src/main/java/test001/map/Solution.java
@@ -0,0 +1,12 @@
+package test001.map;
+
+import java.io.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+// Main class should be named 'Solution'
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
