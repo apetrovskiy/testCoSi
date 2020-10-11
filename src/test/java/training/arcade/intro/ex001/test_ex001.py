a2470d25d42abf7278e244f03ce73a5abdb23dc0
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-10-11 21:52:55 +0300

    .

--- src/test/java/training/arcade/intro/ex001/test_ex001.py
+++ src/test/java/training/arcade/intro/ex001/test_ex001.py
@@ -1,3 +1,11 @@
+import unittest
+from ex001 import add
+
+
 class Ex001Test(unittest.TestCase):
-    def test_ex001():
-        assert(ex001.add(1, 2) = 3)
+    def test_ex001(self):
+        self.assertEqual(add(1, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
