7000ae89700fa476077c4ab19802f95d2d240a62
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-10-24 22:24:53 +0300

    .

--- src/test/java/test_cur_0002/05-alphabeticShift/test_task0005.py
+++ src/test/java/test_cur_0002/05-alphabeticShift/test_task0005.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
-# from solution import 
-from src.main.java.test_cur_0002.05alphabeticShift.solution import alphabeticShift
+from src.main.java.test_cur_0002.a05alphabeticShift.solution import alphabeticShift
 
 
 class Task0005Test(TestCase):
