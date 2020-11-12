87831b1535f7d2fc63d4c22ae4f53ee25fb9aefd
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-12 03:01:21 +0300

    06

--- src/main/java/virt/sse0001/sql/06-filmLibrary/solution.sql
+++ src/main/java/virt/sse0001/sql/06-filmLibrary/solution.sql
@@ -1,3 +1,11 @@
+-- not yet tested
+
+    SELECT sa.actor, aa.age
+    FROM starring_actors sa
+    JOIN actor_ages aa ON aa.actor = sa.actor
+    where sa.movie_name in (select m2.movie from movies m2 join (select m1.genre from movies m1 group by m1.genre order by count(m1.genre) desc limit 1) m3 on m2.genre = m3.genre)
+    ORDER BY aa.age DESC;
+
 /*
 CREATE PROCEDURE filmLibrary()
 BEGIN
