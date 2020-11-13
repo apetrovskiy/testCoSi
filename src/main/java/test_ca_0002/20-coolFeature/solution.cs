515040291c47770ac267f1da8dbcf07769bd255c
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-13 04:19:22 +0300

    .

--- src/main/java/test_ca_0002/20-coolFeature/solution.cs
+++ src/main/java/test_ca_0002/20-coolFeature/solution.cs
@@ -1,11 +1,17 @@
 namespace coolFeature
 {
+    using System;
+    using System.Linq;
 
     public class Solution
     {
         public int[] coolFeature(int[] a, int[] b, int[][] query)
         {
-            return new int[]{1};
+            var updatedB = Enumerable.Range(0, query.Length - 1)
+            .Select(index => 3 == query[index].Length ? query[index][2] : b[index]).ToArray<int>();
+            // updatedB.ToList().ForEach(item => Console.WriteLine(item));
+            var targetValues = query.Select(item => 3 == item.Length ? item[2] : item[1]).Distinct().ToArray<int>();
+            return new int[] { 1 };
         }
     }
 }
