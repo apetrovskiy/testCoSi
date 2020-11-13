d970334d1ce61878b98900e8ab7c01618b0dbff9
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-13 03:23:44 +0300

    .

---
+++ src/main/java/test_ca_0002/20-coolFeature/solution.cs
@@ -0,0 +1,11 @@
+namespace coolFeature
+{
+
+    public class Solution
+    {
+        public int[] coolFeature(int[] a, int[] b, int[][] query)
+        {
+            return new int[]{1};
+        }
+    }
+}
---
+++ src/test/java/test_ca_0002/20-coolFeature/solution_test.cs
@@ -0,0 +1,15 @@
+namespace coolFeature {
+    using Xunit;
+
+    public class SolutionTest{
+        [Fact]
+        public void Test001() {
+            Assert.Equal(new int[]{2,1}, new Solution().coolFeature(new int[]{1, 2, 3},new int[]{3, 4},new int[][]{new int[]{1, 5}, new int[]{0, 0, 1}, new int[]{1, 5}}));
+        }
+
+        [Fact]
+        public void Test002() {
+            Assert.Equal(new int[]{3,4}, new Solution().coolFeature(new int[]{1, 2, 2},new int[]{2, 3},new int[][]{new int[]{1, 4}, new int[]{0, 0, 3}, new int[]{1, 5}}));
+        }
+    }
+}
