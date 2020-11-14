93287b1c20decfec12df5b717ea19d5c3ca961d9
Alexander <apetrovskiy@hotmail.com>
2020-11-15 02:54:16 +0300

    Update SolutionTest.cs

--- src/test/java/test_ca_0002/20-coolFeature/SolutionTest.cs
+++ src/test/java/test_ca_0002/20-coolFeature/SolutionTest.cs
@@ -7,13 +7,23 @@ namespace coolFeature
         [Fact]
         public void Test001()
         {
-            Assert.Equal(new int[] { 2, 1 }, new Solution().coolFeature(new int[] { 1, 2, 3 }, new int[] { 3, 4 }, new int[][] { new int[] { 1, 5 }, new int[] { 0, 0, 1 }, new int[] { 1, 5 } }));
+            Assert.Equal(
+                new int[] { 2, 1 },
+                new Solution().coolFeature(
+                    new int[] { 1, 2, 3 },
+                    new int[] { 3, 4 },
+                    new int[][] { new int[] { 1, 5 }, new int[] { 0, 0, 1 }, new int[] { 1, 5 } }));
         }
 
         [Fact]
         public void Test002()
         {
-            Assert.Equal(new int[] { 3, 4 }, new Solution().coolFeature(new int[] { 1, 2, 2 }, new int[] { 2, 3 }, new int[][] { new int[] { 1, 4 }, new int[] { 0, 0, 3 }, new int[] { 1, 5 } }));
+            Assert.Equal(
+                new int[] { 3, 4 },
+                new Solution().coolFeature(
+                    new int[] { 1, 2, 2 },
+                    new int[] { 2, 3 },
+                    new int[][] { new int[] { 1, 4 }, new int[] { 0, 0, 3 }, new int[] { 1, 5 } }));
         }
     }
 }
