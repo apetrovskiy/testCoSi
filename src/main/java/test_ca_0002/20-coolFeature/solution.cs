1342ae2e80940872b5bb9d2d0f0882bbb53297ca
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-14 18:39:36 +0300

    .

--- src/main/java/test_ca_0002/20-coolFeature/solution.cs
+++ src/main/java/test_ca_0002/20-coolFeature/solution.cs
@@ -11,6 +11,14 @@ namespace coolFeature
             .Select(index => 3 == query[index].Length ? query[index][2] : b[index]).ToArray<int>();
             // updatedB.ToList().ForEach(item => Console.WriteLine(item));
             var targetValues = query.Select(item => 3 == item.Length ? item[2] : item[1]).Distinct().ToArray<int>();
+            var resultCollection = targetValues
+            .Select(val => a
+                .Where(a => a <= val)
+                .Select(a => updatedB
+                    .Where(b => b == val - a)
+                    .Count())
+                .Count())
+            .ToArray<int>();
             return new int[] { 1 };
         }
     }
