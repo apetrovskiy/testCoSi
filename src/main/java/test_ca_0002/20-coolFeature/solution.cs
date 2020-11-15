509f6da4cb74a8ea23593ecb05f27147bab447ba
Alexander <apetrovskiy@hotmail.com>
2020-11-15 04:06:07 +0300

    Update solution.cs

--- src/main/java/test_ca_0002/20-coolFeature/solution.cs
+++ src/main/java/test_ca_0002/20-coolFeature/solution.cs
@@ -6,26 +6,26 @@ namespace coolFeature
     {
         public int[] coolFeature(int[] a, int[] b, int[][] query)
         {
-            /*
-            var updatedB = Enumerable.Range(0, query.Length - 1)
-            .Select(index => 3 == query[index].Length ? query[index][2] : b[index]).ToArray<int>();
-            // updatedB.ToList().ForEach(item => Console.WriteLine(item));
-            var targetValues = query.Select(item => 3 == item.Length ? item[2] : item[1]).Distinct().ToArray<int>();
-            var resultCollection = targetValues
-            .Select(val => a
-                .Where(a => a <= val)
-                .Select(a => updatedB
-                    .Where(b => b == val - a)
-                    .Count())
-                .Count())
-            .ToArray<int>();
-            */
-            var result = query.Select(queryArrayElement => 3 == queryArrayElement.Length ? b[queryArrayElement[1]] = queryArrayElement[2] : calculateQuery(a, b, queryArrayElement)).ToArray<int>();
-            // return new int[] { 1 };
-            return result;
+            var result = query
+                .Select(queryArrayElement =>
+                {
+                    if (3 == queryArrayElement.Length)
+                    {
+                        b[queryArrayElement[1]] = queryArrayElement[2];
+                    }
+                    return 3 == queryArrayElement.Length && 0 == queryArrayElement[0] ?
+                        -1 :
+                        calculateQuery(a, b, queryArrayElement[1]);
+                    }
+                )
+                .ToArray<int>();
+            return result
+                .Where(resultItem => resultItem >= 0)
+                .ToArray<int>();
         }
-        private int calculateQuery(int[] array1, int[] array2, int[] currentQuery){
-            return 1;
+        private int calculateQuery(int[] array1, int[] array2, int currentTarget)
+        {
+            return array1.Select(array1Item => array2.Count(array2Item => currentTarget - array1Item == array2Item)).Sum();
         }
     }
 }
