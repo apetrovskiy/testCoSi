0f40eab79a3d7245b3ee138815ef377c5cc64722
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-18 21:55:54 +0300

    .

--- src/test/java/test_ssre_0003/a01_spiderMove/SolutionTest.java
+++ src/test/java/test_ssre_0003/a01_spiderMove/SolutionTest.java
@@ -19,8 +19,52 @@ public class SolutionTest {
 
     static Stream<Arguments> getInputData() {
         return Stream.of(
-            of(),
-            of()
+            of(2, -2, 6),
+            of(-3, 0, 1),
+            of(0, 0, 1),
+            of(0, 10, 1),
+            of(5, 7, 792)
         );
     }
 }
+
+/*
+Test 1
+Input:
+x: 2
+y: -2
+Expected Output:
+6
+Click the "Run Tests" button to see output and console logs.
+
+Test 2
+Input:
+x: -3
+y: 0
+Expected Output:
+1
+Click the "Run Tests" button to see output and console logs.
+
+Test 3
+Input:
+x: 0
+y: 0
+Expected Output:
+1
+Click the "Run Tests" button to see output and console logs.
+
+Test 4
+Input:
+x: 0
+y: 10
+Expected Output:
+1
+Click the "Run Tests" button to see output and console logs.
+
+Input:
+x: 5
+y: 7
+Expected Output:
+792
+Click the "Run Tests" button to see output and console logs.
+*/
