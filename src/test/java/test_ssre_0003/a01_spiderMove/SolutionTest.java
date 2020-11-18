3f6c15c952f8ae69177fb7670c7dab33a6ca1507
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-11-18 21:50:07 +0300

    .

--- src/test/java/test_ssre_0003/a01_spiderMove/SolutionTest.java
+++ src/test/java/test_ssre_0003/a01_spiderMove/SolutionTest.java
@@ -1,10 +1,26 @@
 package test_ssre_0003.a01_spiderMove;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class SolutionTest {
     @ParameterizedTest
     @MethodSource("getInputData")
     void test(int x, int y, int expectedResult) {
-        val actualResult = new Solution().spiderMove(x,y);
-        
+        final int actualResult = new Solution().spiderMove(x,y);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    static Stream<Arguments> getInputData() {
+        return Stream.of(
+            of(),
+            of()
+        );
     }
 }
