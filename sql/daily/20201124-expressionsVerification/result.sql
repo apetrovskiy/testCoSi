eab22bb807b7054c91941824621dc518d9396b85
Alexander <apetrovskiy@hotmail.com>
2020-11-29 02:57:12 +0300

    Update result.sql

--- sql/daily/20201124-expressionsVerification/result.sql
+++ sql/daily/20201124-expressionsVerification/result.sql
@@ -1,4 +1,13 @@
 CREATE PROCEDURE expressionsVerification()
 BEGIN
 	/* Write your SQL here. Terminate each statement with a semicolon. */
+    SELECT *
+    FROM expressions
+    WHERE c = CASE operation
+    WHEN '+' THEN a + b
+    WHEN '-' THEN a - b
+    WHEN '*' THEN a * b
+    WHEN '/' THEN a / b
+    END
+    ORDER BY id;
 END
