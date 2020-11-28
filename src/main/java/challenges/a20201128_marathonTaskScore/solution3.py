4c3cc913766a8fa15c0df5c596ace47194d69b63
Alexander <apetrovskiy@hotmail.com>
2020-11-29 00:32:47 +0300

    finished

--- src/main/java/challenges/a20201128_marathonTaskScore/solution3.py
+++ src/main/java/challenges/a20201128_marathonTaskScore/solution3.py
@@ -1,2 +1,10 @@
 def marathonTaskScore(marathonLength, maxScore, submissions, successfulSubmissionTime):
-    return 0
+    final_score = maxScore
+    final_score = final_score - successfulSubmissionTime * (maxScore / 2) * (1 / marathonLength)
+    if submissions > 1:
+        final_score = final_score - (submissions - 1) * 10
+    if final_score < maxScore / 2:
+        final_score = maxScore / 2
+    if successfulSubmissionTime == -1:
+        final_score = 0
+    return final_score
--- src/test/java/challenges/a20201128_marathonTaskScore/test_marathon_task_score.py
+++ src/test/java/challenges/a20201128_marathonTaskScore/test_marathon_task_score.py
@@ -8,6 +8,8 @@ import pytest
         (100, 400, 4, 30, 310),
         (100, 400, 95, 30, 200),
         (100, 400, 95, -1, 0),
+        (100, 1000, 1, 99, 505),
+        (100, 1000, 2, 99, 500),
     ],
 )
 def test_solution_marathon_task_score(marathon_length: int, max_score: int, submissions: int, successful_submission_time: int, expected_result: int):
