e7793390d8da0ec22c8f88e972e421efebc458dc
Alexander <apetrovskiy@hotmail.com>
2020-11-29 03:57:07 +0300

    Update solution3.py

--- src/main/java/challenges/a20201123_fancyRide/solution3.py
+++ src/main/java/challenges/a20201123_fancyRide/solution3.py
@@ -1,6 +1,6 @@
-CARS = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"]
+CARS = {1: "UberX", 2: "UberXL", 3: "UberPlus", 4: "UberBlack", 5: "UberSUV"}
 AMOUNT = 20
 
 def fancyRide(l, fares):
-    candidates = [x if fare * l <= AMOUNT for x in fares]
-    print(candidates)
+    candidates = [fare for fare in fares if fare * l <= AMOUNT]
+    return CARS[len(candidates)]
