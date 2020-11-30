d7eac16ceb1d57fcadc729620d24f56a67a496dc
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-11-30 10:31:39 +0300

    Update test_combinationSum.py

--- src/test/java/challenges/a20200530_combinationSum/test_combinationSum.py
+++ src/test/java/challenges/a20200530_combinationSum/test_combinationSum.py
@@ -10,6 +10,7 @@ from src.main.java.challenges.a20200530_combinationSum.solution3 import combinat
         ([2, 4, 6, 8], 8, "(2 2 2 2)(2 2 4)(2 6)(4 4)(8)"),
         ([8, 1, 8, 6, 8], 12, "(1 1 1 1 1 1 1 1 1 1 1 1)(1 1 1 1 1 1 6)(1 1 1 1 8)(6 6)"),
         ([7, 2, 6, 5], 16, "(2 2 2 2 2 2 2 2)(2 2 2 2 2 6)(2 2 2 5 5)(2 2 5 7)(2 2 6 6)(2 7 7)(5 5 6)"),
+        # TODO: more tests
     ],
 )
 def test_combinationSum(input_array: List[int], sum: int, expected_result: str):
