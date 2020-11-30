b08439bb267641de78e4e4ea04a21010fd635a41
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-11-30 11:13:08 +0300

    preparation and mock run

--- src/main/java/challenges/a20200530_combinationSum/solution3.py
+++ src/main/java/challenges/a20200530_combinationSum/solution3.py
@@ -1,2 +1,9 @@
 def combinationSum(a, sum):
-    return "(1)"
+    sections = []
+    for element in a:
+        sections.extend(build_sections(element, a, sum))
+    sections.sort()
+    return ''.join(sections)
+
+def build_sections(elt, input_array, target):
+    return ["(1 1, )(2 2 2)"]
