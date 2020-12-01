5445a0bf2433392e7ed6b1dcd720b83ed0c0fdff
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-02 00:22:22 +0300

    Update solution3.py

--- src/main/java/challenges/a20200530_combinationSum/solution3.py
+++ src/main/java/challenges/a20200530_combinationSum/solution3.py
@@ -1,9 +1,12 @@
 def combinationSum(a, sum):
     sections = []
-    for element in a:
-        sections.extend(build_sections(element, a, sum))
+    for current_element in a:
+        sections.extend(build_sections(current_element, a, sum))
     sections.sort()
-    return ''.join(sections)
+    return ''.join(sections) if len(sections) > 0 else "Empty"
 
-def build_sections(elt, input_array, target):
-    return ["(1 1, )(2 2 2)"]
+def build_sections(element, input_array, target):
+    # return [] if element > target else ["(1 1 1)(2 2 2)"]
+    if element> target:
+        return []
+    return ["(1 1)"] 
