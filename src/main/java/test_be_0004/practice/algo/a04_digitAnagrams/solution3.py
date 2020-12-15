f801b96503b32dff8771b75d917019bf7e7eb360
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-12-15 23:37:48 +0300

    Update solution3.py

--- src/main/java/test_be_0004/practice/algo/a04_digitAnagrams/solution3.py
+++ src/main/java/test_be_0004/practice/algo/a04_digitAnagrams/solution3.py
@@ -12,6 +12,7 @@ def number_of_anagrams(array: List[int], index: int):
     result = 0
     number = array[index]
     for item_index in range(index + 1, len(array)):
-        if ''.join(sorted(str(array[item_index]))) == ''.join(sorted(str(number))):
+        # if ''.join(sorted(str(array[item_index]))) == ''.join(sorted(str(number))):
+        if sorted(str(array[item_index])) == sorted(str(number)):
             result += 1
     return result
