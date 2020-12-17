070b442c5615b8aa76d91bdc1386d3fd1adb7e9f
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-17 21:21:49 +0300

    Update test_running_water.py

--- src/test/java/challenges/a20201217_runningWater/test_running_water.py
+++ src/test/java/challenges/a20201217_runningWater/test_running_water.py
@@ -5,7 +5,17 @@ from typing import List
 
 test_data = [
     ([1, 1, 1], 2, True),
+    ([16, 5, 3], 20, False),
+    ([80, 61, 76], 80, True),
+    ([1, 50, 100], 144, True),
+    ([7, 4, 7], 15, False),
+    ([19, 44, 67], 113, False),
+    ([8, 5, 3], 10, True),
+    ([2, 4, 8], 13, False),
+    ([31, 46, 61], 51, True),
+    ([57, 30, 11], 65, True),
 ]
+
 
 @pytest.mark.parametrize("cap,volume,expected_result", test_data)
 def test_running_water(cap: List[int], volume: int, expected_result: bool):
