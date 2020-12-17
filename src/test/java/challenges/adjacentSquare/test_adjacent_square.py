09ce025a1501003129b8f7c60954bce4b8023817
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-17 18:55:59 +0300

    Update test_adjacent_square.py

--- src/test/java/challenges/adjacentSquare/test_adjacent_square.py
+++ src/test/java/challenges/adjacentSquare/test_adjacent_square.py
@@ -4,7 +4,17 @@ import pytest
 
 test_data = [
     ([12, 5, 4, 13], [5, 4, 12, 13]),
+    ([1, 2, 3], []),
+    ([1, 1, 1, 3, 3, 3], [1, 3, 1, 3, 1, 3]),
+    ([1, 3, 6, 15], [6, 3, 1, 15]),
+    ([1, 3, 6, 8], [6, 3, 1, 8]),
+    ([1, 3, 6, 8, 24], []),
+    ([5, 4, 3, 1, 0], [3, 1, 0, 4, 5]),
+    ([1, 0, 0, 0], [0, 0, 0, 1]),
+    ([10, 15, 1], [1, 15, 10]),
+    ([2, 7, 9, 0, 16, 14], [0, 16, 9, 7, 2, 14]),
 ]
+
 
 @pytest.mark.parameterize("input_array,expected_result", test_data)
 def test_adjacent_square(input_array: List[int], expected_result: List[int]):
