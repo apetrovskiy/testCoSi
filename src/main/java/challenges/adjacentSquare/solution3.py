4b32e360111d7aea70674d2a72c8ef9018d6ca19
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-17 20:01:46 +0300

    Update solution3.py

--- src/main/java/challenges/adjacentSquare/solution3.py
+++ src/main/java/challenges/adjacentSquare/solution3.py
@@ -1,2 +1,12 @@
 def adjacentSquare(a):
-    return []
+    result = []
+    possible_pairs = []
+    
+    for x in a:
+        possible_pairs += [(x, y) if x <= y else (y, x) for y in a if pow(round(math.sqrt(x + y)), 2) == x + y]
+    possible_pairs.sort()
+    possible_pairs = set(possible_pairs)
+
+    print(possible_pairs)
+    
+    return result
