f81fda05e2004cc5b661097489e9009f326d2fbb
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-19 12:09:56 +0300

    Update solution3.py

--- src/main/java/challenges/spiderMove/solution3.py
+++ src/main/java/challenges/spiderMove/solution3.py
@@ -2,7 +2,7 @@ def spiderMove(x, y):
     if x == 0 or y == 0:
         return 1
     m = d = 1
-    for xval in range(1, abs(x) + 1):
-        m *= (xval + abs(y))
-        d *= xval
+    for current_x in range(1, abs(x) + 1):
+        m *= (current_x + abs(y))
+        d *= current_x
     return m /d
