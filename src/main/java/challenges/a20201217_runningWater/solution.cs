a2dc49746630ef74aba00cd1341ccbb501af2f5b
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-26 01:30:06 +0300

    Update solution.cs

--- src/main/java/challenges/a20201217_runningWater/solution.cs
+++ src/main/java/challenges/a20201217_runningWater/solution.cs
@@ -1,5 +1,10 @@
-namespace challenges.a20201217_runningWater{
-class Solution{
-bool runningWater(int[] cap, int volume) {
-    return false;
-}}}
+namespace challenges.a20201217_runningWater
+{
+    class Solution
+    {
+        bool runningWater(int[] cap, int volume)
+        {
+            return false;
+        }
+    }
+}
