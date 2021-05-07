2becec165dd572892c2513723fe3a0f120260c31
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-08 01:43:18 +0300

    PMD

--- src/test/java/mutateArr/TestMutator.java
+++ src/test/java/mutateArr/TestMutator.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@SuppressWarnings("PMD")
 public class TestMutator {
     static Stream getInputData() {
         return Stream.of(
