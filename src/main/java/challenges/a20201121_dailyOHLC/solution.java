57a4440fb9deac697f70c32a144504c5fb8e7e87
Alexander <apetrovskiy@hotmail.com>
2021-05-09 22:31:48 +0300

    PMD

--- src/main/java/challenges/a20201121_dailyOHLC/solution.java
+++ src/main/java/challenges/a20201121_dailyOHLC/solution.java
@@ -1,6 +1,7 @@
 package challenges.a20201121_dailyOHLC;
 
 class Solution {
+    @SuppressWarnings("PMD")
     String[][] dailyOHLC(int[] timestamp, String[] instrument, String[] side, double[] price, int[] size) {
         return new String[][] {};
     }
--- src/main/java/challenges/a20201128_isBull/solution.java
+++ src/main/java/challenges/a20201128_isBull/solution.java
@@ -1,6 +1,7 @@
 package challenges.a20201128_isbull;
 
 class Solution {
+    @SuppressWarnings("PMD")
     boolean isBull(boolean[][] adj) {
         return false;
     }
