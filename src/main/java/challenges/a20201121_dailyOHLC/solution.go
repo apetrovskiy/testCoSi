b64a797ec548c5eb38a0c0c89a4b5d2a6b196c38
Alexander <apetrovskiy@hotmail.com>
2021-05-09 20:27:27 +0300

    Update solution.go

--- src/main/java/challenges/a20201121_dailyOHLC/solution.go
+++ src/main/java/challenges/a20201121_dailyOHLC/solution.go
@@ -1,5 +1,5 @@
 package a20201121_dailyOHLC
 
 func dailyOHLC(timestamp []int, instrument []string, side []string, price []float64, size []int) [][]string {
-	return make([][]string, "")
+	return make([][]string, 0)
 }
