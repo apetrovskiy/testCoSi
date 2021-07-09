ab9d4c31904c56c278f0cfdbee14992feeb5c595
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-07-09 22:16:57 +0300

    Update seats_in_theater.test.js

--- src/main/java/training/core/SeatsInTheater/seats_in_theater.test.js
+++ src/main/java/training/core/SeatsInTheater/seats_in_theater.test.js
@@ -2,6 +2,10 @@ const { seatsInTheater } = require("./seats_in_theater");
 
 describe.each([
     [16, 11, 5, 3, 96],
+    [1, 1, 1, 1, 0],
+    [13, 6, 8, 3, 18],
+    [60, 100, 60, 1, 99],
+    [1000, 1000, 1000, 1000, 0],
 ])(
     "candies(%o, %i) should equal %i",
     (nCols, nRows, col, row, expectedResult) => {
