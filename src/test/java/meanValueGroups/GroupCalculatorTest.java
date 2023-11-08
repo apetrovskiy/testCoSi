d90bd4cb5b92513622bc00f90b220fa5089ea9db
Aleksandr Petrovskiy <uiautomation@hotmail.com>
2023-11-08 16:02:26 +0300

    experimental changes

--- src/test/java/meanValueGroups/GroupCalculatorTest.java
+++ src/test/java/meanValueGroups/GroupCalculatorTest.java
@@ -1,15 +1,29 @@
 package meanValueGroups;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import java.util.Arrays;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@Epic("Training")
+@Feature("Core")
+@Story("Java")
+@DisplayName("Java")
+@Tag("Java")
+@TestInstance(PER_CLASS)
 @SuppressWarnings("PMD")
 public class GroupCalculatorTest {
     @Disabled
     @Test
+    @DisplayName("Mean groups")
     void shouldReturnMeanGroups() {
         final var inputArray = new int[][] {{3, 3, 4, 2}, {4, 4}, {4, 0, 3, 3}, {2, 3}, {3, 3, 3}};
         final var expectedResult = new int[][] {{0, 4}, {1}, {2, 3}};
@@ -22,6 +36,7 @@ public class GroupCalculatorTest {
     }
 
     @Test
+    @DisplayName("just to show")
     void show() {
         // final var inputArray = new int[]{3,
         // 4,
--- src/test/java/training/core/LargestNumber/LargestNumberJavaTest.java
+++ src/test/java/training/core/LargestNumber/LargestNumberJavaTest.java
@@ -1,21 +1,35 @@
 package training.core.LargestNumber;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class LargestNumberTest {
+@Epic("Training")
+@Feature("Core")
+@Story("Java")
+@DisplayName("Largest number")
+@Tag("Java")
+@TestInstance(PER_CLASS)
+class LargestNumberJavaTest {
     @ParameterizedTest
     @MethodSource("getInputData")
+    @DisplayName("Largest number test")
     void shouldCalculateNumber(int input, int expectedResult) {
         assertEquals(expectedResult, new Solution().largestNumber(input));
     }
 
-    static Stream<Arguments> getInputData() {
+    Stream<Arguments> getInputData() {
         return Stream.of(of(1, 9), of(2, 99), of(3, 999), of(9, 999_999_999));
     }
 }
